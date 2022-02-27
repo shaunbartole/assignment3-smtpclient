40259b7dabd792d0b2053582642aae736e64859c
shaunbartole <82050408+shaunbartole@users.noreply.github.com>
2022-02-26 22:02:37 -0500

    test

--- solution.py
+++ solution.py
@@ -46,7 +46,6 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     sendData = "Subject: Hope This Works\r\n"
     clientSocket.send(sendData.encode())
     clientSocket.send(msg.encode())
-    clientSocket.send(endmsg.encode())
     recv5 = clientSocket.recv(1024).decode()
 
     # Message ends with a single period, send message end and handle server response.
